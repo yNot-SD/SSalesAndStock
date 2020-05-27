15df15679932f59df82b86093ed3d8bc9f356c95
yNot-SD <tonysduarte101@gmail.com>
2020-05-26 22:45:26 -0300

    Style da tela de login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerx">
+
+      <div className="boxLogo">
+        <div className="logo">
+          <img src="./assets/logoCS.png" alt="logocs" />
+          <div className="textCS">
+            <h1>CARAMELLO</h1>
+            <h2>SHOES</h2>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="boxLogin">
+          <h1>Entrar no Sistema</h1>
+
+        <i className="fa fa-user" aria-hidden="true"></i>
+        <input className="inputLogin" type="text" placeholder="Login" />
+
+        <i className="fa fa-lock" aria-hidden="true"></i>
+        <input className="inputLogin" type="text" placeholder="Senha" />
+
+        <button>Entrar</button>
+      </div>
+
+      <footer className="footer">
+        <p>CREATED BY TONY S.</p>
+      </footer>
     </div>
   );
 }
