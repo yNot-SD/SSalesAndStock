e116ffa3283ac01ad775076fcb39012d2010e25a
yNot-SD <tonysduarte101@gmail.com>
2020-05-28 17:05:50 -0300

    Separacao dos componentes

--- front/src/App.js
+++ front/src/App.js
@@ -1,52 +1,30 @@
 import React from 'react';
-import './App.css';
+import BoxLogin from './components/BoxLogin.js'
+import BoxLogo from './components/BoxLogo.js'
+
+import './style/App.css';
+import './style/login.css';
+import './style/logo.css';
 
 function App() {
-  return (
-    <div className="containerx">
+    return (
+        <div className="containerx">
+            <div className="header">
+                <h1>Use o que te faz feliz!</h1>
+            </div>
 
-      <div id="bolinhas"></div>
+            <BoxLogo></BoxLogo>
 
-      {/* <header className="header">
-      </header> */}
-        <div className="use">
-          <h1>Use o que te faz feliz</h1>
-        <div className="traco"></div>
-        </div>
-        
+            <div className="hr"></div>
+            
+            <BoxLogin></BoxLogin>
 
-      <div className="boxLogo">
-        <div className="logo">
-          <img src="./assets/logoCS.png" alt="logocs" />
+            <footer className="footer">
+                <p>CREATED BY TONY S.</p>
+            </footer>
+        </div >
 
-
-          <div className="textCS">
-            <h1>CARAMELLO</h1>
-            <h2>SHOES</h2>
-          </div>
-
-        </div>
-      </div>
-
-
-      <div className="boxLogin">
-        <h1>Entrar no Sistema</h1>
-
-        <i className="fa fa-user" aria-hidden="true"></i>
-        <input className="inputLogin" type="text" placeholder="Login" />
-
-        <i className="fa fa-lock" aria-hidden="true"></i>
-        <input className="inputLogin" type="text" placeholder="Senha" />
-
-        <button>Entrar</button>
-      </div>
-
-      <footer className="footer">
-        <p>CREATED BY TONY S.</p>
-      </footer>
-    </div>
-
-  );
+    );
 }
 
 export default App;
