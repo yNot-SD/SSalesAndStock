2e549c68019898e869067532c411d21a269cb538
yNot-SD <tonysduarte101@gmail.com>
2020-05-28 01:11:48 -0300

    Alteracoes e adicao do bordao - teste

--- front/src/App.js
+++ front/src/App.js
@@ -5,19 +5,32 @@ function App() {
   return (
     <div className="containerx">
 
+      <div id="bolinhas"></div>
+
+      {/* <header className="header">
+      </header> */}
+        <div className="use">
+          <h1>Use o que te faz feliz</h1>
+        <div className="traco"></div>
+        </div>
+        
+
       <div className="boxLogo">
         <div className="logo">
           <img src="./assets/logoCS.png" alt="logocs" />
+
+
           <div className="textCS">
             <h1>CARAMELLO</h1>
             <h2>SHOES</h2>
           </div>
+
         </div>
       </div>
 
 
       <div className="boxLogin">
-          <h1>Entrar no Sistema</h1>
+        <h1>Entrar no Sistema</h1>
 
         <i className="fa fa-user" aria-hidden="true"></i>
         <input className="inputLogin" type="text" placeholder="Login" />
@@ -32,6 +45,7 @@ function App() {
         <p>CREATED BY TONY S.</p>
       </footer>
     </div>
+
   );
 }
 
